b0d9972c9a0a7bde91756f187347bd6130ff3465
Nathaniel Richards <nate.richards96@gmail.com>
2021-09-23 19:30:43 -0700

    Factorial Push

---
+++ Factorial/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int factorial = 5;
+            int product = 1;
+            for (int i = 1; i <= 5; i++)
+            {
+                product *= i;
+            }
+            Console.WriteLine("$Factorial is  {0}",product);
+            Console.WriteLine(DateTime.Now);
+        }
+    }
+}
